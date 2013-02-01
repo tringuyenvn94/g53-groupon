47ef2ac1b6f4da63b982b1c37e3aadbd9092844e
tranthe.khtn <tranthe.khtn@gmail.com>
2013-02-01 17:11:34 +0000

    V7 sua ket noi linq, up lan 1

---  g53-groupon --username tranthe.khtn@gmail.com/Customer_Guest/Product_Detail.aspx.cs
+++  g53-groupon --username tranthe.khtn@gmail.com/Customer_Guest/Product_Detail.aspx.cs
@@ -9,6 +9,28 @@ public partial class Customer_Guest_Product_Detail : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (!IsPostBack)
+        {
+            if (Request.QueryString["ID"] != null)
+            {
+                Chi_Tiet_San_Pham ct = Chi_Tiet_San_Pham_BLL.Tim_Theo_Ma(Request.QueryString["ID"]);
+            }
+        }
+        Load_CT_SP();
+    }
+    public void Load_CT_SP()
+    {
+        Chi_Tiet_San_Pham ct = Chi_Tiet_San_Pham_BLL.Tim_Theo_Ma("DL00000003");
+        Label_TSP.Text = ct.TenSanPham;
+        Label_GB.Text = Label_GB2.Text = string.Format("{0:0,00}", ct.donGiaBan);
+        Label_GKM.Text = Label_GKM2.Text = string.Format("{0:0,000}",ct.donGiaKhuyenMai);
+        Label_Ti_Le_Giam.Text = Label_TL2.Text = string.Format("{0:0.}",(ct.donGiaKhuyenMai / ct.donGiaBan) * 100);
+        Label_MT.Text = ct.moTa;
+        Label_SNM.Text = ct.soLuotMua.ToString();
+        Image_Top.ImageUrl = "../Images_Product/hinh san pham 3.png";
+        Tai_Khoan tk = Tai_Khoan_BLL.Tim_Theo_Ma(San_Pham_BLL.Tim_Theo_Ma(ct.maSanPham).IDDoiTac);
+        Label_DoiTac.Text = tk.hoTen_tenCongTy;
+        Label_DiaChiDoiTac.Text = tk.diaChi;
+        Label_DienThoaiDoiTac.Text = tk.dienThoai;
     }
 }
---  g53-groupon --username tranthe.khtn@gmail.com/Customer_Guest/Register.aspx.cs
+++  g53-groupon --username tranthe.khtn@gmail.com/Customer_Guest/Register.aspx.cs
@@ -9,6 +9,17 @@ public partial class Customer_Guest_Register : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+    }
+    protected void Button_DK_Click(object sender, EventArgs e)
+    {
+        Tai_Khoan tk = new Tai_Khoan();
+        tk.hoTen_tenCongTy = TextBox_Ho_Ten.Text;
+        tk.userName = tk.email = TextBox_Email.Text;
+        tk.passWord = TextBox_MK.Text;
+        tk.maLoaiNguoiDung = "KH";
+        if (!Tai_Khoan_BLL.Dang_Ky_Tai_Khoan(tk))
+            Label_TB.Text = "Có lổi xãy ra trong quá trinh đăng ký";
+        else
+            Label_TB.Text = "Ok";
     }
 }
